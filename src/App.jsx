6f0d79d8feb3d4a13b8cb69bc51d8675b4bef4f1
Vincent Fuseau <vincent.fuseau@hotmail.fr>
2025-02-27 19:24:08 +0100

    Ajout d'une animation astronaute dans la page 404 et du scss associé.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "@/pages/Home";
 import Error from "@/pages/Error";
+import "@/styles/main.scss";
 
 const App = () => {
   return (

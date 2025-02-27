d542f72c8bfb5971c36852b4a413b10866aad523
Vincent Fuseau <vincent.fuseau@hotmail.fr>
2025-02-27 18:27:28 +0100

    Initialisation de l'app et paramétrage du projet.

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+      <footer>
+        <p>©2020 Kasa. All {"\n"}rights reserved</p>
+      </footer>
+    );
+  }
+  
+  export default Footer;
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,15 @@
+import Header from "@/components/Header"
+import Footer from "@/components/Footer"
+
+const Home =() => {
+    return (
+        <div>
+            <Header />
+            <h1>Home Page</h1>
+            <p>Bienvenue to the Home Page</p>
+            <Footer />
+        </div>
+    )
+};
+
+export default Home;

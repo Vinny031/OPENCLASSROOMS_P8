98a5f02e2a391c63658ab9ada5f38f399fce9f01
Vincent Fuseau <vincent.fuseau@hotmail.fr>
2025-03-02 13:05:25 +0100

    Ajout des icones pour les social media (jsx et lien vers le jsx du footer) ainsi que de leur style. Lien vers le style du footer.

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,11 @@
+import SocialIcons from "../Social"
+
 const Footer = () => {
     return (
       <footer className="footer">
+        <SocialIcons />
         <p>©2025 Site forgé par Vincent Fuseau | Tous droits réservés</p>
       </footer>
     );
-  }
-  
-  export default Footer;
+}
+export default Footer;
---
+++ src/components/Social/index.jsx
@@ -0,0 +1,33 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub} from '@fortawesome/free-brands-svg-icons';
+
+const SocialIcons = () => {
+  return (
+    <ul className="social-icons">
+      <li className="icon-content">
+        <a
+          href="https://www.linkedin.com/in/vincent-fuseau-173795296/"
+          aria-label="LinkedIn"
+          data-social="linkedin"
+        >
+          <div className="filled"></div>
+          <FontAwesomeIcon icon={faLinkedin} className="icon" />
+        </a>
+        <div className="tooltip">LinkedIn</div>
+      </li>
+      <li className="icon-content">
+        <a
+          href="https://github.com/Vinny031?tab=repositories"
+          aria-label="GitHub"
+          data-social="github"
+        >
+          <div className="filled"></div>
+          <FontAwesomeIcon icon={faGithub} className="icon" />
+        </a>
+        <div className="tooltip">GitHub</div>
+      </li>
+    </ul>
+  );
+};
+
+export default SocialIcons;

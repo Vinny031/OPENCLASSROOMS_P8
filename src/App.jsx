57dbf3b85254e513227319b9b985cded6e27a8d8
Vincent Fuseau <vincent.fuseau@hotmail.fr>
2025-03-30 08:15:05 +0200

    Modification du code du header pour ajouter un reponsive tablet et mobile.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "@/pages/Home/Home.jsx";
 import Error from "@/pages/Error/Error.jsx";
 import "@/styles/main.scss";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const App = () => {
   return (

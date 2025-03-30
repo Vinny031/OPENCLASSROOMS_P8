220c91881a35d7b9a5b4b7de5d3908487c86580b
Vincent Fuseau <vincent.fuseau@hotmail.fr>
2025-03-30 09:35:09 +0200

    Modification de la section about pour ajotuer un effet flou et une gestion du click pour clarifier le contenu de l'infocard.

--- src/components/Section_About/About_Infotext/About_Infotext.jsx
+++ src/components/Section_About/About_Infotext/About_Infotext.jsx
@@ -1,23 +1,23 @@
-import './About_Infotext.scss'
+import React from 'react';
+import './About_Infotext.scss';
 
-const About_Infotext = () => {
-    return (
-      <div className="about_infotext">
-        <p className="about_infotext_text">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. 
-          Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. 
-          Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. 
-          Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. 
-          Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. 
-          Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. 
-          Praesent egestas leo in pede. Praesent blandit odio eu enim. 
-          Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus 
-          et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. 
-          Maecenas adipiscing ante non diam sodales hendrerit.
-        </p>
-      </div>
-    );
-  };
-  
-  export default About_Infotext;
-  
+const About_Infotext = ({ isBlurred }) => {
+  return (
+    <div className="about-infotext">
+      <p className={`about-infotext-text ${isBlurred ? 'blurred' : ''}`}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.
+        Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. 
+        Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. 
+        Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. 
+        Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. 
+        Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. 
+        Praesent egestas leo in pede. Praesent blandit odio eu enim. 
+        Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus 
+        et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. 
+        Maecenas adipiscing ante non diam sodales hendrerit.
+      </p>
+    </div>
+  );
+};
+
+export default About_Infotext;
--- src/components/Section_About/Section_About.jsx
+++ src/components/Section_About/Section_About.jsx
@@ -1,18 +1,31 @@
-import React from 'react';
-import About_Card from './About_Card/About_Card.jsx'
-import About_Infotext from './About_Infotext/About_Infotext.jsx'
+import React, { useState, useEffect } from 'react';
+import About_Card from './About_Card/About_Card.jsx';
+import About_Infotext from './About_Infotext/About_Infotext.jsx';
 import About_Attachment from './About_Attachment/About_Attachment.jsx';
-import './Section_About.scss'
+import './Section_About.scss';
 
 const About = () => {
+  // Initialiser isBlurred à true pour flouter le texte au départ
+  const [isBlurred, setIsBlurred] = useState(true);
+
+  // Fonction pour basculer l'état du flou
+  const toggleBlur = () => {
+    setIsBlurred((prevState) => !prevState);
+  };
+
+  // Appliquer un effet au premier chargement (pour s'assurer que le texte est flouté dès le début)
+  useEffect(() => {
+    setIsBlurred(true);
+  }, []);
+
   return (
-    <section className="about" id='about'>
-      <div className='section_about_title'>
+    <section className="about" id="about">
+      <div className="section_about_title">
         <h2>À propos</h2>
       </div>
       <div className="about_content">
-        <About_Card />
-        <About_Infotext />
+        <About_Card toggleBlur={toggleBlur} />
+        <About_Infotext isBlurred={isBlurred} />
         <About_Attachment />
       </div>
     </section>
